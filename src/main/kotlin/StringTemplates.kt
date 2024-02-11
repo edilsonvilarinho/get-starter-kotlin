4dcf386563b67c395de71aaf02828670964a2105
Edilson Souza Vilarinho Messias <edilson.vilarinho.messias@gmail.com>
2024-02-10 22:36:37 -0300

    feat:added Variables and String Templates

---
+++ src/main/kotlin/StringTemplates.kt
@@ -0,0 +1,16 @@
+package org.example
+
+fun main() {
+
+    val customers = 10
+
+    println(" There are $customers customers")
+    println(" There are ${customers + 1} customers")
+
+
+    val name = "Mary"
+    val age = 20
+
+    println(" $name is $age years old ")
+
+}
---
+++ src/main/kotlin/Variables﻿.kt
@@ -0,0 +1,9 @@
+package org.example
+
+fun main() {
+    //val read only
+    val popcorn = 5
+    val hotdog = 7
+    //val mutable
+    var custumers = 10
+}
